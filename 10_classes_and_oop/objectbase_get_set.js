7ee3d09a9466779644cfde1e76a8f97c3c5df772
Bhavik3012 <bhaviksongara301205@gmail.com>
2025-01-08 21:19:57 +0530

    objectbase_get_set.js

--- 10_classes_and_oop/objectbase_get_set.js
+++ 10_classes_and_oop/objectbase_get_set.js
@@ -1,3 +1,14 @@
 const User = {
-    
-}
+  _email: "b@bc.com",
+  _password: "abc",
+
+  get email() {
+    return this._email.toUpperCase();
+  },
+  set email(value) {
+    this._email = value;
+  },
+};
+
+const tea = Object.create(User);
+console.log(tea.email);
